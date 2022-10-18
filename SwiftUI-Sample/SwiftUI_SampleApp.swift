e371411710ec71c4b6e5cd1ef44b6c5da750ebeb
masukura yutaka <masukura.yutaka@opst.co.jp>
2022-10-18 16:28:57 +0900

    Initial Commit

---
+++ SwiftUI-Sample/SwiftUI_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SampleApp.swift
+//  SwiftUI-Sample
+//
+//  Created by 増倉豊 on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
